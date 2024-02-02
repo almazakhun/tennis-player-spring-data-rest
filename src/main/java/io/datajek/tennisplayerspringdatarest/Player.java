e8ffee27011e644ab1857f378f33687e62e75c2b
almaz <almaz180599@gmail.com>
2024-02-01 22:05:57 -0500

    tennis-player-spring-data-rest

---
+++ src/main/java/io/datajek/tennisplayerspringdatarest/Player.java
@@ -0,0 +1,24 @@
+package io.datajek.tennisplayerspringdatarest;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String name;   
+    private String nationality;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private Date birthDate;
+    private int titles;
+    //constructors
+    //getters and setters
+}
---
+++ src/main/java/io/datajek/tennisplayerspringdatarest/PlayerRepository.java
@@ -0,0 +1,6 @@
+package io.datajek.tennisplayerspringdatarest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository <Player, Integer> {
+}
